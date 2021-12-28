9e78452f169500b6850356856712ff9cdb850d6c
Sean Shields <stshields@seans-mbp.lan>
2021-12-28 13:34:01 -0500

    hello world

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-    // Import discord.js and create the client
-    const Discord = require('discord.js')
-    const client = new Discord.Client();
-    
-    // Register an event so that when the bot is ready, it will log a messsage to the terminal
-    client.on('ready', () => {
-      console.log(`Logged in as ${client.user.tag}!`);
-    })
-    
-    // Register an event to handle incoming messages
-    client.on('message', async msg => {
-      // This block will prevent the bot from responding to itself and other bots
-      if(msg.author.bot) {
-        return
-      }
-    
-      // Check if the message starts with '!hello' and respond with 'world!' if it does.
-      if(msg.content.startsWith("!hello")) {
-        msg.reply("world!")
-      }
-    })
-    
-    // client.login logs the bot in and sets it up for use. You'll enter your token here.
-    client.login('OTI1NDI1Mzk5MTA4ODk0Nzkx.Ycs7kA.fOPMSgNOAnMlrmaVbNBXqDMhmro');
+// Import discord.js and create the client
+const Discord = require('discord.js')
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+
+// Register an event so that when the bot is ready, it will log a messsage to the terminal
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+})
+
+// Register an event to handle incoming messages
+client.on('messageCreate', async msg => {
+  // This block will prevent the bot from responding to itself and other bots
+  if(msg.author.bot) {
+    return
+  }
+
+  // Check if the message starts with '!hello' and respond with 'world!' if it does.
+  if(msg.content.startsWith("!hello")) {
+    msg.reply("world!")
+  }
+})
+
+// client.login logs the bot in and sets it up for use. You'll enter your token here.
+client.login('OTI1NDI1Mzk5MTA4ODk0Nzkx.Ycs7kA.fOPMSgNOAnMlrmaVbNBXqDMhmro');

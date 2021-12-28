a90dfcec2cc967f681b268881a87ec4b135bc1f3
Sean Shields <sean.t.shields@gmail.com>
2021-12-28 14:48:50 -0500

    get random champ

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-const Discord = require('discord.js')
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const axios = require('axios');
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const champsData = 'http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/champion.json';
+const champions = [];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function fetchChamps() {
+  axios.get(champsData).then((response) => {
+    champions = response;
+  })
+}
+
+async function getRandomChamp(ignore) {
+  if (!champions.length) {
+    await fetchChamps();
+  }
+
+  const randomIndex = getRandomInt(champions.length - 1);
+  return champions[randomIndex]
+}
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-})
+  console.log(`Logged in as ${client.user.tag}`);
+});
 
 client.on('messageCreate', async msg => {
   // dont respond to bots
-  if(msg.author.bot) {
-    return
+  if (msg.author.bot) {
+    return;
   }
 
-  // Check if the message starts with '!hello' and respond with 'world!' if it does.
-  if(msg.content.startsWith("!random")) {
-    console.log(msg)
-    msg.reply("world!")
+  if (msg.content.startsWith("!random")) {
+    const champion = getRandomChamp();
+    msg.reply(champion.name);
   }
-})
+});
 
 // TODO: move to github secret
 client.login('OTI1NDI1Mzk5MTA4ODk0Nzkx.Ycs7kA.fOPMSgNOAnMlrmaVbNBXqDMhmro');
